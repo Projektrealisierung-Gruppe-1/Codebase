c394838df1c4e2764692a441c2b3078dda6e8aa4
coreprog <crdhbw@gmail.com>
2023-07-25 12:13:04 +0200

    changes

--- webapplikation/classification.py
+++ webapplikation/classification.py
@@ -1,6 +1,8 @@
 from transformers import pipeline, AutoTokenizer
 import streamlit as st
+from summarizer import Summarizer
 
+@st.cache_data(show_spinner=False)
 def classfier(text):
     class_model_path = "bert-base-uncased"
     tokenizer=AutoTokenizer.from_pretrained(class_model_path,use_fast=False)
@@ -10,15 +12,21 @@ def classfier(text):
 
     return predict
 
+@st.cache_data(show_spinner=False)
 def sentiment(text):
-    sentiment_model_path = "cardiffnlp/twitter-roberta-base-sentiment-latest"
+    sentiment_model_path = "siebert/sentiment-roberta-large-english"
     tokenizer=AutoTokenizer.from_pretrained(sentiment_model_path,use_fast=False)
     sentiment_task = pipeline("sentiment-analysis", model=sentiment_model_path, tokenizer=tokenizer)
 
     preddict = sentiment_task(text)[0]
-    print(preddict["score"])
     prob = round(preddict["score"]*100,2)
-    print(prob)
     pred = preddict["label"]
 
     return prob, pred
+
+@st.cache_data(show_spinner=False)
+def txtsummary(text, crate):
+    model = Summarizer('distilbert-base-uncased')
+    zsm_text = model(text, ratio=crate)
+
+    return zsm_text
--- webapplikation/textverarbeitung.py
+++ webapplikation/textverarbeitung.py
@@ -1,7 +1,5 @@
 import streamlit as st
-from classification import classfier,sentiment
-from speech_to_text import transcribe_audio_imp
-from io import StringIO
+from classification import classfier,sentiment,txtsummary
 import docx2txt
 import textract
 import pandas as pd
@@ -9,6 +7,11 @@ from PyPDF2 import PdfReader
 from dotenv import load_dotenv
 import os
 import deepl
+
+from transformers import pipeline
+from st_custom_components import st_audiorec
+import streamlit as st
+import os
 
 
 def text_page():
@@ -26,6 +29,7 @@ def text_page():
     st.write("---")
 
     if optch == "Dateiupload":
+        txt = ""
         uploaded_file = st.file_uploader("Choose a file",accept_multiple_files=False,type=['docx', 'pdf', 'txt','doc'])
         if uploaded_file is not None:
             st.write(uploaded_file.type)
@@ -34,7 +38,8 @@ def text_page():
             st.write(uploaded_file.name[-3:])
             # check uploaded file for data type
             if "ocx" in uploaded_file.name[-3:]:
-                st.write("docx erkannt")           
+                st.write("docx erkannt")   
+
                 txt = docx2txt.process(uploaded_file)
                 st.write(txt)
                 
@@ -47,7 +52,6 @@ def text_page():
                 st.write("pdf erkannt")           
  
                 reader = PdfReader(uploaded_file)
-                txt = ""
                 for i in range(len(reader.pages)):
                     content = reader.pages[i].extract_text()
                     txt = txt + content
@@ -56,70 +60,88 @@ def text_page():
                 
             if "txt" in uploaded_file.name[-3:]:
                 st.write("txt erkannt")  
-                txt = ""
+
                 for line in uploaded_file:
                     txt = txt + "\n" + line.decode("utf-8")
                 st.write(txt)
                 
+
     elif optch == "Textfeld":
         txt = st.text_area("Your text:",key= "NLP")
+        load_dotenv()
+        DEEPLKEY = os.getenv("DEEPLKEY")
+        translator = deepl.Translator(DEEPLKEY)
+        target_language = "EN-US"
+    
+        if txt != "":
+            txt = str(translator.translate_text(txt, target_lang=target_language))
+            if txt != "":
+                st.write(f"""Der eingegebene Text auf Englisch ist: \\
+                        {txt}""")
+    
 
     elif optch == "Speech to Text":
         txt = ""
-        start_recording = st.button("Start Recording")
-        stop_recording = st.button("Stop Recording")
 
-        if start_recording:
-            with st.spinner('Bitte sprechen, Text wird erkannt...'):      
-                st.session_state.stop_recording = False
-                txt = transcribe_audio_imp()
+        wav_audio_data = st_audiorec()
 
-        if stop_recording:
-            st.session_state.stop_recording = True
-        if txt == "":
-            pass
-        else:
-            st.write(f"Der erkannte Text ist: {txt}")
-    
+        if wav_audio_data is not None:
+
+            if os.path.isfile('webapplikation/audio.wav'):
+                with open('webapplikation/audio.wav', mode='bw') as f:
+                    f.write(wav_audio_data)
+            # Your error handling goes here
+            else:
+                with open('webapplikation/audio.wav', mode='bx+') as f:
+                    f.write(wav_audio_data)
+            
+            pipe = pipeline("automatic-speech-recognition", model="openai/whisper-small")
+            txt = pipe("webapplikation/audio.wav")["text"]
+            st.write(txt)
+        
     
     
     # translate logic
     # load dotenv secrets
-    load_dotenv()
-    DEEPLKEY = os.getenv("DEEPLKEY")
-    translator = deepl.Translator(DEEPLKEY)
-    target_language = "EN-US"
-    
-    txt_english = translator.translate_text(txt, target_lang=target_language)
-    if txt_english != "":
-        st.write(f"Der übersetzte Text ist: {txt_english}")
-    
+
     
     
 
-    age = st.slider('Kompressionsrate', 20, 80, 60)
+    crate = st.slider('Kompressionsrate', 20, 80, 60)
 
     st.write("---")
     st.header("Analyse des Textes")
-    st.subheader("Klassifizierungen")
 
     if txt != "":
+        st.subheader("Text Zusammenfassung")
+        with st.spinner('Text wird zusammengefasst...'):
+                        zsm_txt = txtsummary(txt,crate/100)
+        st.write(zsm_txt)
+
+        st.markdown("""
+                    
+
+        """)
+        
+        st.subheader("Klassifizierungen")
+
+        #if     
+
         c1,c2 =  st.columns([1, 1])
 
         c1.markdown("**Text Klassifzierung**")
         with st.spinner('Classification wird durchgeführt...'):
-            txtpred = classfier(txt)
+            txtpred = classfier(zsm_txt)
         c1.write(f"Der Text ist aus der Kategorie {txtpred}.")
 
         c2.markdown("**Sentiment Analyse**")
         with st.spinner('Sentiment Analyse wird durchgeführt...'):
-                        prob, pred = sentiment(txt)
-
-
+                        prob, pred = sentiment(zsm_txt)
         c2.write(f"Mit einer Wahrscheinlichkeit von {prob}% sagt das Modell voraus, dass dieser Text {pred} ist.")
 
-        st.subheader("Text Zusammenfassung")
-        st.text("....")
+        
+    else:
+         st.write("Es wurde noch keine Datei hochgeladen.")
     
 
     st.write("---")
@@ -138,7 +160,6 @@ def text_page():
 
         text_contents = '''This is some text'''
         st.download_button('Download Zusammenfassung', text_contents)
-
 
 
 
